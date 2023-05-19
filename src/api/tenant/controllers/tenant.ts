48b798ba24618ccbd042d62c49465ce637cb98b2
Vinicius Oliveira <vinicius.oliveira@Desktop-00293423.local>
2023-05-19 08:24:54 -0300

    return files tenant

--- src/api/tenant/controllers/tenant.ts
+++ src/api/tenant/controllers/tenant.ts
@@ -2,6 +2,24 @@
  * tenant controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::tenant.tenant');
+export default factories.createCoreController(
+  "api::tenant.tenant",
+  ({ strapi }) => ({
+    async find(ctx) {
+      let filters = {};
+      if (ctx.request.query.filters) {
+        filters = ctx.request.query.filters;
+      }
+      const tenants = await strapi.query("api::tenant.tenant").findMany({
+        where: filters,
+        populate: { logo: true, banner: true },
+      });
+
+      return tenants;
+    },
+
+    async findOne(ctx) {},
+  })
+);
