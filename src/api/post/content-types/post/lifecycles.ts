102a63865e091dc470fe713e50374824e9c52ed6
Vinicius Oliveira <vinicius.oliveira@Desktop-00293423.local>
2023-05-24 17:47:56 -0300

    fix: ajuste na mensagem de email

--- src/api/post/content-types/post/lifecycles.ts
+++ src/api/post/content-types/post/lifecycles.ts
@@ -8,7 +8,12 @@ module.exports = {
   },
   async afterCreate(event) {
     const { result, params } = event;
-    console.log(result, params);
+
+    const tenant = await strapi.query("api::tenant.tenant").findOne({
+      where: {
+        id: params.data.tenant,
+      },
+    });
 
     const emailTemplate = {
       subject: `Ouvidoria`,
@@ -16,13 +21,12 @@ module.exports = {
       Sua requisição gerou o protocolo: ${result.protocol}.`,
       html: `<h1>Sua requisição foi gerada com sucesso!</h1>
     <p>Sua requisição gerou o protocolo: <strong>${result.protocol}</strong> <p>
-    <p> Você pode acompanhar a sua requisição através do link: <a href="http://localhost:8082/status">http://localhost:8082/status</a>.</p>`,
+    <p> Você pode acompanhar a sua requisição através do link <a href="https://ouvidoria-app-pv54x.ondigitalocean.app/ouvidoria/status-denuncia/?company=${tenant.identity}">consultar status</a>.</p>`,
     };
 
     await strapi.plugins["email"].services.email.sendTemplatedEmail(
       {
         to: result.email,
-        // from: is not specified, the defaultFrom is used.
       },
       emailTemplate
     );
