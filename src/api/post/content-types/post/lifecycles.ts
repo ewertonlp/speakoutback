c89a3fe269b4ce3d624019886af7c6ca0944d136
Vinicius Oliveira <vinicius.oliveira@Desktop-00293423.local>
2023-05-24 20:14:43 -0300

    fix error send for

--- src/api/post/content-types/post/lifecycles.ts
+++ src/api/post/content-types/post/lifecycles.ts
@@ -7,28 +7,32 @@ module.exports = {
     data.params.data.protocol = uuid();
   },
   async afterCreate(event) {
-    const { result, params } = event;
+    try {
+      const { result, params } = event;
 
-    const tenant = await strapi.query("api::tenant.tenant").findOne({
-      where: {
-        id: params.data.tenant,
-      },
-    });
+      const tenant = await strapi.query("api::tenant.tenant").findOne({
+        where: {
+          id: params.data.tenant,
+        },
+      });
 
-    const emailTemplate = {
-      subject: `Ouvidoria`,
-      text: `Sua requisição foi gerada com sucesso!
+      const emailTemplate = {
+        subject: `Ouvidoria`,
+        text: `Sua requisição foi gerada com sucesso!
       Sua requisição gerou o protocolo: ${result.protocol}.`,
-      html: `<h1>Sua requisição foi gerada com sucesso!</h1>
+        html: `<h1>Sua requisição foi gerada com sucesso!</h1>
     <p>Sua requisição gerou o protocolo: <strong>${result.protocol}</strong> <p>
     <p> Você pode acompanhar a sua requisição através do link <a href="https://ouvidoria-app-pv54x.ondigitalocean.app/ouvidoria/status-denuncia/?company=${tenant.identity}">consultar status</a>.</p>`,
-    };
+      };
 
-    await strapi.plugins["email"].services.email.sendTemplatedEmail(
-      {
-        to: result.email,
-      },
-      emailTemplate
-    );
+      await strapi.plugins["email"].services.email.sendTemplatedEmail(
+        {
+          to: result.email,
+        },
+        emailTemplate
+      );
+    } catch (err) {
+      throw new Error("Error:" + err.message);
+    }
   },
 };
