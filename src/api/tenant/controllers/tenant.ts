a068598eac00dd3f1d7a2238e71c7b3416faaeb2
Vinicius Oliveira <vinicius.oliveira@Desktop-00293423.local>
2023-05-24 22:12:46 -0300

    fix error send for

--- src/api/tenant/controllers/tenant.ts
+++ src/api/tenant/controllers/tenant.ts
@@ -9,20 +9,24 @@ export default factories.createCoreController(
   "api::tenant.tenant",
   ({ strapi }) => ({
     async find(ctx) {
-      const user = await GetTenantUserJwt();
-      if (!user.tenant.id) {
-        return ctx.notAcceptable("user no tenant valid");
-      }
-      let filters = {};
-      if (ctx.request.query.filters) {
-        filters = ctx.request.query.filters;
-      }
-      const tenants = await strapi.query("api::tenant.tenant").findMany({
-        where: filters,
-        populate: { logo: true, banner: true },
-      });
+      try {
+        const user = await GetTenantUserJwt();
+        if (!user?.tenant?.id) {
+          return ctx.badRequest();
+        }
+        let filters = {};
+        if (ctx.request.query.filters) {
+          filters = ctx.request.query.filters;
+        }
+        const tenants = await strapi.query("api::tenant.tenant").findMany({
+          where: filters,
+          populate: { logo: true, banner: true },
+        });
 
-      return tenants;
+        return tenants;
+      } catch (err) {
+        return ctx.unauthorized();
+      }
     },
   })
 );
--- src/utils/tenant.ts
+++ src/utils/tenant.ts
@@ -1,9 +1,14 @@
 export default async function GetTenantUserJwt() {
   const ctx = strapi.requestContext.get();
-  const { id, isAdmin = false } = await strapi.plugins[
+  const tokenUser = await strapi.plugins[
     "users-permissions"
   ].services.jwt.getToken(ctx);
 
+  if (!tokenUser) {
+    throw new Error("token not valid");
+  }
+
+  const { id, isAdmin = false } = tokenUser;
   const user = await strapi.entityService.findOne(
     "plugin::users-permissions.user",
     id,
