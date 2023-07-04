cd0c605eb066bf0d2bbd16a53ef199fdc9d72cb6
Vinicius Oliveira <vinicius@local>
2023-07-04 11:50:03 -0300

    feat: ajuste na permissão de owner para post

--- src/api/postclosed/controllers/postclosed.ts
+++ src/api/postclosed/controllers/postclosed.ts
@@ -19,29 +19,42 @@ export default factories.createCoreController('api::postclosed.postclosed', ({ s
         if (!tenant) {
           return ctx.notAcceptable("Usuário sem tenant");
         }
-
-        const post = await strapi.query("api::post.post").findOne({
+        const userOwner = await strapi.query("plugin::users-permissions.user").findOne({
             where: {
-                id: ctx.request.body.data.post
+                id: ctx.request.body.data.user
             },
             populate: {
-                users: {
+                role: {
                     where: {
-                        id: ctx.request.body.data.user
-                    },
-                    select: ['id']
+                        name: 'admin'
+                    }
                 }
             }
-        })
+          })
 
-        const user = post.users.find(user => user.id == ctx.request.body.data.user)
+          if(!userOwner?.role){
+                const post = await strapi.query("api::post.post").findOne({
+                    where: {
+                        id: ctx.request.body.data.post
+                    },
+                    populate: {
+                        users: {
+                            where: {
+                                id: ctx.request.body.data.user
+                            },
+                            select: ['id']
+                        }
+                    }
+                })
+            
+                const userPost = post.users.find(user => user.id == ctx.request.body.data.user)
 
-        if (!post || !user) {
-            return ctx.notAcceptable("Usuário sem permissão para finalizar o post");
-          }
-       
-        const response = super.create(ctx);
-        return response;
+                if (!post || !userPost) {
+                    return ctx.notAcceptable("Usuário sem permissão para finalizar o post");
+                }
+        }
+        return super.create(ctx);
+        
       } catch (err) {
         return ctx.badRequest(`${err.message}`, JSON.stringify(err));
       }},
@@ -60,29 +73,42 @@ export default factories.createCoreController('api::postclosed.postclosed', ({ s
           if (!tenant) {
             return ctx.notAcceptable("Usuário sem tenant");
           }
-  
-          const postclosed = await strapi.query("api::postclosed.postclosed").findOne({
-              where: {
-                  id: ctx.request.params.id,
-                  tenant: ctx.request.body.data.tenant,
-                  user: ctx.request.body.data.user
-              },
-              populate: {
-                  user: {
-                      where: {
-                          id: ctx.request.body.data.user
-                      },
-                      select: ['id']
-                  }
-              }
-          })
-
-          if (!postclosed || !postclosed?.user?.id ) {
-              return ctx.notAcceptable("Usuário sem permissão para finalizar o post");
+          const userOwner = await strapi.query("plugin::users-permissions.user").findOne({
+            where: {
+                id: ctx.request.body.data.user
+            },
+            populate: {
+                role: {
+                    where: {
+                        name: 'admin'
+                    }
+                }
             }
+          })
+          if(!userOwner?.role){
+            const postclosed = await strapi.query("api::postclosed.postclosed").findOne({
+                where: {
+                    id: ctx.request.params.id,
+                    tenant: ctx.request.body.data.tenant,
+                    user: ctx.request.body.data.user
+                },
+                populate: {
+                    user: {
+                        where: {
+                            id: ctx.request.body.data.user
+                        },
+                        select: ['id']
+                    }
+                }
+            })
+            
+            if (!postclosed || !postclosed?.user?.id ) {
+                return ctx.notAcceptable("Usuário sem permissão para finalizar o post");
+                }
+        }
          
-          const response = super.update(ctx);
-          return response;
+        return super.update(ctx);
+           
         } catch (err) {
           return ctx.badRequest(`${err.message}`, JSON.stringify(err));
         }}
