59447f367f773db8c9a77740355d56bcdd9cf948
Vinicius Oliveira <vinicius@local>
2023-07-05 13:46:24 -0300

    feat: implementação no findOne de users

--- src/api/user/controllers/user.ts
+++ src/api/user/controllers/user.ts
@@ -18,4 +18,18 @@ export default {
       populate: ["tenant", "role"],
     });
   },
+  async findOne(ctx) {
+    const user = await GetTenantUserJwt();
+    if (user.role.name != "admin") {
+      return ctx.notAcceptable("Usuário sem permissão para consultar usuários");
+    }
+
+    return await await strapi.query("plugin::users-permissions.user").findOne({
+      where: {
+        tenant: user.tenant.id,
+        id: ctx.request.params.id,
+      },
+      populate: ["tenant", "role"],
+    });
+  },
 };
