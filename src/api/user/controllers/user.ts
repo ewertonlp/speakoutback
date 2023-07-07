37885d178cf87e2dd4123c92f023ac5fb8edd48a
Vinícius Oliveira <35545235+viniciusc0@users.noreply.github.com>
2023-07-07 12:43:11 -0300

    Update user.ts

--- src/api/user/controllers/user.ts
+++ src/api/user/controllers/user.ts
@@ -45,12 +45,6 @@ export default {
   },
      async findMe(ctx) {
     const user = await GetTenantUserJwt();
-    return await await strapi.query("plugin::users-permissions.user").findOne({
-      where: {
-        tenant: user.tenant.id,
-        id: ctx.request.params.id,
-      },
-      populate: ["tenant", "role", "areas"],
-    });
-  },
+    return user;
+  }
 };
