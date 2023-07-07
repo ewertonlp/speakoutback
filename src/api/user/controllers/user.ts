c6e36be4db7242d6dbe87dd5991e90ff6a111152
Vinícius Oliveira <35545235+viniciusc0@users.noreply.github.com>
2023-07-07 11:55:05 -0300

    Create service me

--- src/api/user/controllers/user.ts
+++ src/api/user/controllers/user.ts
@@ -32,4 +32,15 @@ export default {
       populate: ["tenant", "role", "areas"],
     });
   },
+
+    async me(ctx) {
+    const user = await GetTenantUserJwt();
+    return await await strapi.query("plugin::users-permissions.user").findOne({
+      where: {
+        tenant: user.tenant.id,
+        id: ctx.request.params.id,
+      },
+      populate: ["tenant", "role", "areas"],
+    });
+  },
 };
