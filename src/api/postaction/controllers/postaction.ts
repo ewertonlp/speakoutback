85e6c2ee209b001730f42d85e58b4cb34c880c30
Vinicius Oliveira <vinicius.oliveira@local>
2023-08-08 08:55:02 -0300

    feat: adding releation postCLosed

--- src/api/postaction/controllers/postaction.ts
+++ src/api/postaction/controllers/postaction.ts
@@ -2,6 +2,42 @@
  * postaction controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+import GetTenantUserJwt from "../../../utils/tenant";
 
-export default factories.createCoreController('api::postaction.postaction');
+export default factories.createCoreController(
+  "api::postaction.postaction",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const user = await GetTenantUserJwt();
+      const filter = {
+        tenant: user.tenant.id,
+      };
+      return await strapi.query("api::postclosed.postclosed").findMany({
+        where: filter,
+        populate: {
+          tenant: true,
+          post: true,
+          media: true,
+          postactionsdetails: true,
+        },
+      });
+    },
+    async findOne(ctx) {
+      const user = await GetTenantUserJwt();
+      const filter = {
+        tenant: user.tenant.id,
+        id: ctx.request.params.id,
+      };
+      return await strapi.query("api::postclosed.postclosed").findOne({
+        where: filter,
+        populate: {
+          tenant: true,
+          post: true,
+          media: true,
+          postactionsdetails: true,
+        },
+      });
+    },
+  })
+);
